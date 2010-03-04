71b7b7a592b675d033bbcc719239fe786ed8e514
sgtcoolguy <cwilliams@aptana.com>
2010-03-04 14:40:10 -0500

    convert from shell execution to block execution

--- commands/align_table_cells.rb
+++ commands/align_table_cells.rb
@@ -5,15 +5,8 @@ command 'Save & Align Table Cells' do |cmd|
   cmd.scope = 'text.cucumber.feature'
   cmd.output = :replace_selection
   cmd.input = :selection, :document
-  cmd.invoke =<<-EOF
-#!/usr/bin/env sh
-cat <<'RUBYEOF' > /tmp/cucumber-$$.rb
-
-require ENV['TM_BUNDLE_SUPPORT'] + "/cucumber/mate/table_aligner"
-
-puts(Cucumber::Mate::TableAligner.new.align(STDIN.readlines))
-RUBYEOF
-
-${TM_RUBY:-ruby} -w -Ku /tmp/cucumber-$$.rb; exit_status=$?; rm /tmp/cucumber-$$.rb; exit $exit_status
-EOF
+  cmd.invoke do
+    require ENV['TM_BUNDLE_SUPPORT'] + "/cucumber/mate/table_aligner"    
+    puts(Cucumber::Mate::TableAligner.new.align(STDIN.readlines))
+  end
 end
