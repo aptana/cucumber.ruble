e0fc3a14c9cf15f3cd87fa20da606e7e83f0ae0e
sgtcoolguy <cwilliams@aptana.com>
2010-03-04 14:39:51 -0500

    use stringio objects to get command working

--- commands/run_single_scenario.rb
+++ commands/run_single_scenario.rb
@@ -6,9 +6,12 @@ command 'Run Single Scenario' do |cmd|
   cmd.output = :show_as_html
   cmd.input = :document, :document
   cmd.invoke do
+    require 'stringio'
+    output = StringIO.new
     require ENV['TM_BUNDLE_SUPPORT'] + "/cucumber/mate/feature_helper"
-    runner = Cucumber::Mate::Runner.new(STDOUT, ENV['TM_PROJECT_DIRECTORY'], ENV['TM_FILEPATH'], ENV['TM_CUCUMBER_BIN'], ENV['TM_CUCUMBER_OPTS'])
+    runner = Cucumber::Mate::Runner.new(output, ENV['TM_PROJECT_DIRECTORY'], ENV['TM_FILEPATH'], ENV['TM_CUCUMBER_BIN'], ENV['TM_CUCUMBER_OPTS'])
     
     runner.run_scenario(ENV['TM_LINE_NUMBER'])
+    output.string
   end
 end
